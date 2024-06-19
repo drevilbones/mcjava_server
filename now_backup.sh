7e33509a1bc63f838a1a003fc3ee7d6bcea6d231
Nick Scratch <drevilbones@gmail.com>
2024-06-19 12:54:52 -0400

    backup fixes, new mods, new logs alias

--- now_backup.sh
+++ now_backup.sh
@@ -5,8 +5,7 @@ mc_data="/opt/minecraft/data_java/"
 mc_backup="/big/Archives/mc_backups/"
 maxbackups=30
 
-mc_send () { docker exec mcjava rcon-cli $1; }
-
+mc_send () { sudo docker exec mcjava rcon-cli $1; }
 
 mc_send "say §6Server going down for backups in §415 minutes!"
 sleep 5m
@@ -14,24 +13,24 @@ mc_send "say §6Server going down for backups in §410 minutes!"
 sleep 5m
 mc_send "say §6Server going down for backups in §45 minutes!"
 sleep 5m
+
 mc_send "say §6Server going down for backups §4right now! Byebye!"
 sleep 5
 
-#docker compose down
-mc_send "stop"
-
+cd $mc_comp
+sudo docker compose down
 cd $mc_data
 fname="mc_$(date +%s).tgz"
-tar czvf $fname world
+sudo tar czvf $fname world
 cd $mc_comp
-docker compose up -d
+sudo docker compose up -d
 
-mv $mc_data/$fname $mc_backup
+sudo mv $mc_data/$fname $mc_backup
 
 cd $mc_backup
 numbackups=$(ls | wc -l)
 if [[ numbackups -gt maxbackups ]]
 then
-	ls -1t | tail -n 1 | xargs rm -f
+	ls -1t | tail -n 1 | xargs sudo rm -f
 fi
 
